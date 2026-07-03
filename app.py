f7be37772eaa215c9ec17f475cf6faccc2c39839
Pavan Borade <pavanborade887@gmail.com>
2026-07-03 16:56:24 +0530

    Initial Project Setup:

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Business Intelligence",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("🚀 AI Business Intelligence & Predictive Analytics Platform")
+
+st.write("Welcome Pavan! 🎉")
