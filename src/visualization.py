10f35f1554f89c2647c3fe0c20e6b2b3b11394fd
Pavan Borade <pavanborade887@gmail.com>
2026-07-09 23:41:53 +0530

    Add data visualization module with bar, pie, and line charts

---
+++ src/visualization.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from data_loader import load_data
+
+ # Bar Chart
+
+def region_sales_chart(df):
+    region_sales = df.groupby("Region")["Sales"].sum()
+
+    print(region_sales)
+
+    plt.figure(figsize=(8, 5))
+    region_sales.plot(kind="bar")
+
+    plt.title("Region Wise Sales")
+    plt.xlabel("Region")
+    plt.ylabel("Total Sales")
+
+    plt.show()
+
+# Pie Chart
+
+def category_sales_chart(df):
+    category_sales = df.groupby("Category")["Sales"].sum()
+
+    plt.figure(figsize=(5, 5))
+
+    plt.pie(
+        category_sales,
+        labels=category_sales.index,
+        autopct="%1.1f%%",
+        startangle=90
+    )
+
+    plt.title("Category Wise Sales Distribution")
+
+    plt.show()
+
+# Monthly sales chart [line chart]
+
+def monthly_sales_chart(df):
+    # Convert Order_Date to datetime (safety)
+    df["Order_Date"] = pd.to_datetime(df["Order_Date"])
+
+    # Create Month Name
+    df["Month"] = df["Order_Date"].dt.month_name()
+
+    # Total Sales by Month
+    monthly_sales = df.groupby("Month")["Sales"].sum()
+    month_order = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+]
+    
+    monthly_sales = monthly_sales.reindex(month_order)
+
+    print(monthly_sales)
+
+    plt.figure(figsize=(10, 5))
+
+    monthly_sales.plot(kind="line", marker="o")
+
+    plt.title("Monthly Sales Trend")
+    plt.xlabel("Month")
+    plt.ylabel("Total Sales")
+    plt.xticks(ticks=range(len(monthly_sales.index)),
+           labels=monthly_sales.index,
+           rotation=45)
+
+    plt.grid(True)
+
+    plt.show()
+
+
+# Load Dataset
+df = load_data("data/raw/superstore_sales.csv")
+
+# Call Functions
+region_sales_chart(df)
+category_sales_chart(df)
+monthly_sales_chart(df)
